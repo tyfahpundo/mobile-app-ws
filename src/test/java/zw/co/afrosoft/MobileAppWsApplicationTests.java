9d03961ea5361cfd6136a280b27d4fa1e0b57467
tyfah <tafadzwaallentyfah@gmail.com>
2021-07-28 15:12:35 +0200

    First Commit

---
+++ src/test/java/zw/co/afrosoft/MobileAppWsApplicationTests.java
@@ -0,0 +1,13 @@
+package zw.co.afrosoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileAppWsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
