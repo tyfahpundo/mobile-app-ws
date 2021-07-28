8a38711c1188924e4cc58763647a5e27d06f31cd
tyfah <tafadzwaallentyfah@gmail.com>
2021-07-28 22:46:03 +0200

    Implemented web security

--- src/main/java/zw/co/afrosoft/service/UserService.java
+++ src/main/java/zw/co/afrosoft/service/UserService.java
@@ -1,7 +1,8 @@
 package zw.co.afrosoft.service;
 
+import org.springframework.security.core.userdetails.UserDetailsService;
 import zw.co.afrosoft.dto.UserDto;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
     UserDto createUser(UserDto user);
 }
--- src/main/java/zw/co/afrosoft/service/UserServiceImpl.java
+++ src/main/java/zw/co/afrosoft/service/UserServiceImpl.java
@@ -2,27 +2,44 @@ package zw.co.afrosoft.service;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import zw.co.afrosoft.dto.UserDto;
 import zw.co.afrosoft.model.User;
 import zw.co.afrosoft.persistence.UserRepository;
+import zw.co.afrosoft.utilities.Utils;
 
 @Service
 public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository repo;
+    @Autowired
+    Utils utils;
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
     public UserDto createUser(UserDto userDto) {
+        if(repo.findUserByEmail(userDto.getEmail()) != null) throw new RuntimeException("User Already Exist");
+
         User user = new User();
         BeanUtils.copyProperties(userDto, user);
 
-        user.setEncryptedPassword("test");
-        user.setUserId("testUserID");
+        String publicUserId = utils.generateUserId(30);
+        user.setUserId(publicUserId);
+
+        user.setEncryptedPassword(bCryptPasswordEncoder.encode(userDto.getPassword()));
 
         User storedUser = repo.save(user);
         UserDto returnValue = new UserDto();
         BeanUtils.copyProperties(storedUser,returnValue);
         return returnValue;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return null;
     }
 }
